95ee18dbc1d34d1d189b04ba5953d2096186fd4e
unknown <john@same.com>
2025-11-08 12:54:45 +0200

    added script

---
+++ script.js
@@ -0,0 +1,7 @@
+const FirstNumber = document.getElementById("UserInput1");
+const SecondNumber = document.getElementById("UserInput2");
+const calcButton = document.getElementsById("UserTrigger");
+
+// calcButton.addEventListener('click'; someText){
+//     console.log("Hello, World")
+// }
